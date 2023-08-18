09f31b135a1d3c0427443e2a66006bec24cf136c
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2023-08-18 07:58:24 +0700

    update title tag

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/style.css">
 
-    <title>Aplikasi Blog | {{ $title }}</title>
+    <title>Blog | {{ $title }}</title>
 </head>
 
 <body>
